0961040d84c187e01e3454de995178f3d9d18b84
SceleryHuang <1104243221@qq.com>
2019-06-03 23:48:37 +0800

    router design

--- src/layouts/SiderMenu.vue
+++ src/layouts/SiderMenu.vue
@@ -1,11 +1,78 @@
 <template>
-  <div>
-    菜单
+  <div style="width: 256px">
+    <a-button
+      type="primary"
+      @click="toggleCollapsed"
+      style="margin-bottom: 16px"
+    >
+      <a-icon :type="collapsed ? 'menu-unfold' : 'menu-fold'" />
+    </a-button>
+    <a-menu
+      :defaultSelectedKeys="['1']"
+      :defaultOpenKeys="['2']"
+      mode="inline"
+      :theme="theme"
+      :inlineCollapsed="collapsed"
+    >
+      <template v-for="item in menuData">
+        <a-menu-item v-if="!item.children" :key="item.path">
+          <a-icon v-if="item.meta.icon" type="item.meta.icon" />
+          <span>{{ item.meta.title }}</span>
+        </a-menu-item>
+        <sub-menu v-else :menu-info="item" :key="item.path" />
+      </template>
+    </a-menu>
   </div>
 </template>
 
 <script>
-export default {};
+/*
+ * recommend SubMenu.vue https://github.com/vueComponent/ant-design-vue/blob/master/components/menu/demo/SubMenu.vue
+ * SubMenu1.vue https://github.com/vueComponent/ant-design-vue/blob/master/components/menu/demo/SubMenu1.vue
+ * */
+import SubMenu from "./SubMenu";
+export default {
+  props: {
+    theme: {
+      type: String,
+      default: "dark"
+    }
+  },
+  components: {
+    "sub-menu": SubMenu
+  },
+  data() {
+    const menuData = this.getMenudata(this.$router.options.routes);
+    return {
+      collapsed: false,
+      list: [],
+      menuData
+    };
+  },
+  methods: {
+    toggleCollapsed() {
+      this.collapsed = !this.collapsed;
+    },
+    getMenudata(routes) {
+      const menuData = [];
+      routes.forEach(item => {
+        if (item.name && !item.hideInMenu) {
+          const newItem = { ...item };
+          delete newItem.children;
+          if (item.collapsed && !item.hideChildrenInMenu) {
+            newItem.children = this.getMenudata(item.children);
+          }
+          menuData.push(newItem);
+        } else if (
+          !item.hideInMenu &&
+          !item.hideChildrenInMenu &&
+          item.children
+        ) {
+          menuData.push(...this.getMenudata(item.children));
+        }
+      });
+      return menuData;
+    }
+  }
+};
 </script>
-
-<style></style>
