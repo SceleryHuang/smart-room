c8140553b5db5f3032fa5b08aefa647c438ccc57
SceleryHuang <1104243221@qq.com>
2019-06-11 23:57:38 +0800

    two types of authorities

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -15,6 +15,7 @@
       <a-layout>
         <a-layout-header style="background: #fff; padding: 0">
           <a-icon
+            v-auth="['admin']"
             class="trigger"
             :type="collapsed ? 'menu-unfold' : 'menu-fold'"
             @click="collapsed = !collapsed"
@@ -27,7 +28,10 @@
         </a-layout-footer>
       </a-layout>
     </a-layout>
-    <setting-drawer></setting-drawer>
+    <Authorized :authority="['admin']">
+      <setting-drawer></setting-drawer>
+    </Authorized>
+
     <router-view></router-view>
   </div>
 </template>
